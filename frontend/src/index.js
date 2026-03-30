ef1e004e27b3477a78bb7c6219f3fa486a6008eb
Rjrohan <rjrohan0340@gmail.com>
2026-03-30 22:31:58 +0530

    Add complete CloudProof implementation with scoring engine and security features

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useParams, Navigate } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Register from './Register';
@@ -15,7 +15,8 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Navigate to="/demo1" replace />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/:username" element={<UserRoute />} />
       </Routes>
     </BrowserRouter>
